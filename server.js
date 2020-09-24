13f14c3690a1fa4acf0080c24d2cb4cfc1975725
Murugesan <PMurugesan@ad.harman.com>
2020-09-24 16:25:26 +0530

    added cors

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 //Install express server
 
 const express = require('express');
+var cors = require('cors');
 const path = require ('path');
 
 const app = express();
-
+app.use(cors());
 app.use(express.static(__dirname + '/dist/ecom_backend'));
 
 app.get('*', function(req, res) {
