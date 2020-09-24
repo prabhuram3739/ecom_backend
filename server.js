8b81ffdd9498b7f9bc498de29808d33df1fb9622
Murugesan <PMurugesan@ad.harman.com>
2020-09-24 16:48:08 +0530

    Added gitignore

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const path = require ('path');
 
 const app = express();
 app.use(cors());
-app.use(express.static(__dirname + '/dist/ecom_backend'));
+//app.use(express.static(__dirname + '/dist/ecom_backend'));
 
-app.get('*', function(req, res) {
+/*app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname+ '/dist/ecom_backend/index.html'));
-});
+});*/
 
 app.listen(process.env.PORT || 8080);
